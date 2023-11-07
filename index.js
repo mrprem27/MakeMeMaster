74fff09e1c60e784a3028f24c2e95f1947864a38
Prem Purushottam Bhati <prem_12013110@nitkkr.ac.in>
2023-11-07 23:30:08 +1300

    second

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: "10mb", extented: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extented: true }));
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://makememaster.netlify.app'],
     credentials: true
 }));
 app.use('/uploads', express.static('uploads'))
